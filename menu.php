a46dba1081d61bb1c7a24f6a9436c1654cad1d2f
Alisson <alisson_lima_souza@yahoo.com.br>
2018-10-11 21:18:05 -0300

    Versão 1

---
+++ menu.php
@@ -0,0 +1,18 @@
+<div>
+    <ul>
+        <li><a href="index.php?pagina=1">Cadastro jogador</a></li>
+        <li><a href="index.php?pagina=2">Cadastro time</a></li>
+        <li><a href="index.php?pagina=3">Criação de elenco</a></li>
+        <li><a href="index.php?pagina=4">Cadastro de calendário de jogos</a></li>
+        <li><a href="index.php?pagina=5">Súmula da partida</a></li>
+        <li><a href="index.php?pagina=0">Relatório de time</a></li>
+        <li><a href="index.php?pagina=7">Relatório do jogador</a></li>
+        <li><a href="index.php?pagina=0">Ranking dos times</a></li>
+
+
+
+
+
+
+    </ul>
+</div>
