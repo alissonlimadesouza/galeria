f71ca2d80b1860c47116d7b8e529a68694574afa
Alisson <alisson_lima_souza@yahoo.com.br>
2018-10-13 16:00:57 -0300

    Versão 1

---
+++ index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <style>
+            @import url('css/galeria.css');
+        </style>
+        <script>
+            function img1() {
+                document.getElementById('img1').style.visibility = "visible";
+              //  document.getElementById('img2').style.visibility = "hidden";
+              //  document.getElementById('img3').style.visibility = "hidden";
+                document.getElementById("img1").style.transform = "rotateY(-360deg)";
+               
+            }
+            function img2() {
+                document.getElementById('img2').style.visibility = "visible";
+               // document.getElementById('img1').style.visibility = "hidden";
+               // document.getElementById('img3').style.visibility = "hidden";
+                document.getElementById("img2").style.transform = "rotate(360deg)";
+                document.getElementById("img2").style.marginLeft = "300px";
+            }
+            function img3() {
+                document.getElementById("img3").style.transform = "rotate(360deg)";
+                document.getElementById('img3').style.visibility = "visible";
+               
+              //  document.getElementById('img2').style.visibility = "hidden";
+                document.getElementById("img3").style.marginLeft = "600px";
+
+
+            }
+
+            var valor;
+            valor = 0;
+            var recursiva = function () {
+
+                setTimeout(recursiva, 1200);
+
+                if (valor == 1) {
+                  
+                   
+
+                    setTimeout(img1, 3000);
+                    document.getElementById("img1").style.transform = "rotateY(360deg)";
+
+                } else if (valor == 2) {
+
+                    setTimeout(img2, 3000);
+                    document.getElementById("img2").style.transform = "rotate(-360deg)";
+                    document.getElementById("img2").style.marginLeft = "900px";
+                    document.getElementById("img3").style.marginLeft = "900px";
+                     document.getElementById("img1").style.transform = "rotateX(360deg)";
+
+                } else if (valor == 3) {
+                    document.getElementById("img3").style.transform = "rotate(-360deg)";
+                    setTimeout(img3, 4000);
+                    document.getElementById("img2").style.marginLeft = "0px";
+                     document.getElementById("img1").style.transform = "rotateY(360deg)";
+
+
+
+                } else if (valor == 4) {
+                    valor = 0;
+                }
+
+                valor++;
+
+
+            }
+        </script>
+    </head>
+    <body onload="recursiva()">
+        <input type="button" value="imagem 1" onclick="img1()" >
+        <input type="button" value="imagem 2" onclick="img2()" >
+        <input type="button" value="imagem 3" onclick="img3()" >
+        <div id="img1">
+
+        </div>
+        <div id="img2">
+
+        </div>
+        <div id="img3">
+
+        </div>
+    </body>
+</html>
