b480d00d98838c907008dc0eb4a07378ed86966c
Alisson <alisson_lima_souza@yahoo.com.br>
2018-10-20 13:16:40 -0300

    Versão 1.1 - Alteração no comentário para autor Alisson Lima de Souza

--- servicos/Conexao.php
+++ servicos/Conexao.php
@@ -1,16 +1,8 @@
-
 <?php
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  * Description of Conexao
  *
- * @author Administrador
+ * @author Alisson lima de souza
  */
 class ConexaoProjeto {
 
