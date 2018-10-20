a01f0e41f18336b7ee61e054e18fc489e91b5722
Alisson <alisson_lima_souza@yahoo.com.br>
2018-10-20 13:12:54 -0300

    Versão 1.1 - Criação de funções contabilizando apenas os times sumulados.

--- servicos/contabilizarCartoesVermelhos.php
+++ servicos/contabilizarCartoesVermelhos.php
@@ -1,4 +1,5 @@
 <?php
+
 /*
  * Programação verifica a quantidade de cartões amarelos
  * e se for de tres para cima contabiliza na 
@@ -8,56 +9,71 @@
  */
 
 
-$contabilizar = new ConexaoProjeto($_local, $_senha, $_usuario, $_banco);
+function contabilizarVermelhos($idTime) {
 
-$consulta = "select * from jogadores";
-$contagem = $contabilizar->contaOcorrencias($consulta);
-$idJogador = $contabilizar->listaOcorrencia($consulta, "idJogador");
+    include './variaveisGlobais.php';
+    $contabilizar = new ConexaoProjeto($_local, $_senha, $_usuario, $_banco);
+    $consulta = "select * from jogadores";
+    $contagem = $contabilizar->contaOcorrencias($consulta);
+    $idJogador = $contabilizar->listaOcorrencia($consulta, "idJogador");
 
 
-for ($i = 0; $i < $contagem; $i++) {
 
-    $consulta = "SELECT sum(ththc.quantidadeCartoes) as vermelhos FROM time_has_torneio_has_cartoes ththc
+
+
+    for ($i = 0; $i < $contagem; $i++) {
+
+        $consulta = "SELECT sum(ththc.quantidadeCartoes) as vermelhos FROM time_has_torneio_has_cartoes ththc
 inner JOIN jogadores j 
 ON
 j.idJogador=ththc.IdJogador
+inner JOIN times_has_torneios tht 
+ON
+tht.idTimeHasTorneiro=ththc.idTimeHasTorneio
+INNER JOIN times t 
+ON
+t.idTime=tht.idTime
+OR
+t.idTime=tht.idTimeB
 WHERE
 ththc.idCartao=1
 AND
-j.idJogador='$idJogador[$i]'";
-    $contagemVermelhos = $contabilizar->contaOcorrencias($consulta);
-    $quantidadeVermelhos = $contabilizar->listaOcorrencia($consulta, "vermelhos");
+j.idJogador='$idJogador[$i]'
+AND
+t.idTime='$idTime'";
+        $contagemVermelhos = $contabilizar->contaOcorrencias($consulta);
+        $quantidadeVermelhos = $contabilizar->listaOcorrencia($consulta, "vermelhos");
 
 
 
-    for ($j = 0; $j < $contagemVermelhos; $j++) {
-        echo "codigo jogador: $idJogador[$i] - quantidades amarelos: $quantidadeVermelhos[$j] ";
+        for ($j = 0; $j < $contagemVermelhos; $j++) {
+            echo "codigo jogador: $idJogador[$i] - quantidades vermelhos: $quantidadeVermelhos[$j] ";
 
-        if ($quantidadeVermelhos[$j] >= 1) {
-            echo 'irá calcular';
+            if ($quantidadeVermelhos[$j] >= 1) {
+                echo 'irá calcular';
 
-            $consulta = "select totalVermelho from jogadores where idJogador=$idJogador[$i]";
+                $consulta = "select totalVermelho from jogadores where idJogador=$idJogador[$i]";
 
-            $buscaVermelhos = $contabilizar->listaOcorrencia($consulta, "totalVermelho");
+                $buscaVermelhos = $contabilizar->listaOcorrencia($consulta, "totalVermelho");
 
-            foreach ($buscaVermelhos as $value) {
-                
-                $auxiliar=$value;
-                
-                $totalVermelhos = $auxiliar + $quantidadeVermelhos[$j];
-                
-                $consultaAtualiza = "UPDATE jogadores SET totalVermelho=$totalVermelhos WHERE idJogador='$idJogador[$i]'";
-                $contabilizar->editarOcorrencia($consultaAtualiza);
-                
-                $consultaDeleta="DELETE FROM time_has_torneio_has_cartoes WHERE IdJogador='$idJogador[$i]' AND idCartao=1";
-                $contabilizar->excluiOcorrencia($consultaDeleta);
-                
-                
+                foreach ($buscaVermelhos as $value) {
+
+                    $auxiliar = $value;
+
+                    $totalVermelhos = $auxiliar + $quantidadeVermelhos[$j];
+
+                    $consultaAtualiza = "UPDATE jogadores SET totalVermelho=$totalVermelhos WHERE idJogador='$idJogador[$i]'";
+                    $contabilizar->editarOcorrencia($consultaAtualiza);
+
+                    $consultaDeleta = "DELETE FROM time_has_torneio_has_cartoes WHERE IdJogador='$idJogador[$i]' AND idCartao=1";
+                    $contabilizar->excluiOcorrencia($consultaDeleta);
+                }
             }
-        }
 
-        echo '<br />';
-        
-   
+            echo '<br />';
+        }
     }
 }
+
+contabilizarVermelhos($idTimeA);
+contabilizarVermelhos($idTimeB);
