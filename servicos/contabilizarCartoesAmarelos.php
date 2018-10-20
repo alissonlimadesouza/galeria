7008fb79451f8f713f35332919f6255c29b77124
Alisson <alisson_lima_souza@yahoo.com.br>
2018-10-20 13:12:38 -0300

    Versão 1.1 - Criação de funções contabilizando apenas os times sumulados.

--- servicos/contabilizarCartoesAmarelos.php
+++ servicos/contabilizarCartoesAmarelos.php
@@ -1,10 +1,5 @@
 <?php
 
-
-
-
-
-
 /*
  * Programação verifica a quantidade de cartões amarelos
  * e se for de tres para cima contabiliza na 
@@ -15,55 +10,74 @@
 include './variaveisGlobais.php';
 include './Conexao.php';
 
-$contabilizarAmarelo = new ConexaoProjeto($_local, $_senha, $_usuario, $_banco);
+$idTimeA = $_POST['idTimeA'];
+$idTimeB = $_POST['idTimeB'];
 
-$consulta = "select * from jogadores";
-$contagem = $contabilizarAmarelo->contaOcorrencias($consulta);
-$idJogador = $contabilizarAmarelo->listaOcorrencia($consulta, "idJogador");
+function contabilizarAmarelos($idTime) {
+    include './variaveisGlobais.php';
+    $contabilizarAmarelo = new ConexaoProjeto($_local, $_senha, $_usuario, $_banco);
+    $consulta = "select * from jogadores";
+    $contagem = $contabilizarAmarelo->contaOcorrencias($consulta);
+    $idJogador = $contabilizarAmarelo->listaOcorrencia($consulta, "idJogador");
 
 
-for ($i = 0; $i < $contagem; $i++) {
 
-    $consulta = "SELECT sum(ththc.quantidadeCartoes) as amarelos FROM time_has_torneio_has_cartoes ththc
+
+
+
+    for ($i = 0; $i < $contagem; $i++) {
+
+        $consulta = "SELECT sum(ththc.quantidadeCartoes) as amarelos FROM time_has_torneio_has_cartoes ththc
 inner JOIN jogadores j 
 ON
 j.idJogador=ththc.IdJogador
+inner JOIN times_has_torneios tht 
+ON
+tht.idTimeHasTorneiro=ththc.idTimeHasTorneio
+INNER JOIN times t 
+ON
+t.idTime=tht.idTime
+OR
+t.idTime=tht.idTimeB
 WHERE
 ththc.idCartao=2
 AND
-j.idJogador='$idJogador[$i]'";
-    $contagemAmarelos = $contabilizarAmarelo->contaOcorrencias($consulta);
-    $quantidadeAmarelos = $contabilizarAmarelo->listaOcorrencia($consulta, "amarelos");
+j.idJogador='$idJogador[$i]'
+AND
+t.idTime='$idTime'";
+        $contagemAmarelos = $contabilizarAmarelo->contaOcorrencias($consulta);
+        $quantidadeAmarelos = $contabilizarAmarelo->listaOcorrencia($consulta, "amarelos");
 
 
 
-    for ($j = 0; $j < $contagemAmarelos; $j++) {
-        echo "codigo jogador: $idJogador[$i] - quantidades amarelos: $quantidadeAmarelos[$j] ";
+        for ($j = 0; $j < $contagemAmarelos; $j++) {
+            echo "codigo jogador: $idJogador[$i] - quantidades amarelos: $quantidadeAmarelos[$j] ";
 
-        if ($quantidadeAmarelos[$j] >= 3) {
-            echo 'irá calcular';
+            if ($quantidadeAmarelos[$j] >= 3) {
+                echo 'irá calcular';
 
-            $consulta = "select totalAmarelo from jogadores where idJogador=$idJogador[$i]";
+                $consulta = "select totalAmarelo from jogadores where idJogador=$idJogador[$i]";
 
-            $buscaAmarelos = $contabilizarAmarelo->listaOcorrencia($consulta, "totalAmarelo");
+                $buscaAmarelos = $contabilizarAmarelo->listaOcorrencia($consulta, "totalAmarelo");
 
-            foreach ($buscaAmarelos as $value) {
-                
-                $auxiliar=$value;
-                
-                $totalAmarelos = $auxiliar + $quantidadeAmarelos[$j];
-                
-                $consultaAtualiza = "UPDATE jogadores SET totalAmarelo=$totalAmarelos WHERE idJogador='$idJogador[$i]'";
-                $contabilizarAmarelo->editarOcorrencia($consultaAtualiza);
-                
-                $consultaDeleta="DELETE FROM time_has_torneio_has_cartoes WHERE IdJogador='$idJogador[$i]'";
-                $contabilizarAmarelo->excluiOcorrencia($consultaDeleta);
-                
-                
+                foreach ($buscaAmarelos as $value) {
+
+                    $auxiliar = $value;
+
+                    $totalAmarelos = $auxiliar + $quantidadeAmarelos[$j];
+
+                    $consultaAtualiza = "UPDATE jogadores SET totalAmarelo=$totalAmarelos WHERE idJogador='$idJogador[$i]'";
+                    $contabilizarAmarelo->editarOcorrencia($consultaAtualiza);
+
+                    $consultaDeleta = "DELETE FROM time_has_torneio_has_cartoes WHERE IdJogador='$idJogador[$i]'";
+                    $contabilizarAmarelo->excluiOcorrencia($consultaDeleta);
+                }
             }
-        }
 
-        echo '<br />';
+            echo '<br />';
+        }
     }
 }
 
+contabilizarAmarelos($idTimeA);
+contabilizarAmarelos($idTimeB);
